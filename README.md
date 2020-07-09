c10ca2359a24c6a71d1eb1d14ab0b0742e50555a
Emrys Ingersoll <ingersoll@gmail.com>
2020-07-09 14:53:40 -0700

    MariaDB Destination

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
-# Quasar Plugin MariaDB [![Discord](https://img.shields.io/discord/373302030460125185.svg?logo=discord)](https://discord.gg/pSSqJrr)
+# Quasar MariaDB/MySQL Plugins [![Discord](https://img.shields.io/discord/373302030460125185.svg?logo=discord)](https://discord.gg/pSSqJrr)
 
-## Usage
+## Destination
 
-```sbt
-libraryDependencies += "com.precog" %% "quasar-plugin-mariadb" % <version>
+The MariaDB/MySQL destination plugin enables Quasar to load data into MariaDB, MySQL or other protocol-compliant stores. Loading is done via the `LOAD DATA LOCAL INFILE` statement to achieve good performance.
+
+### Destination Configuration
+
+JSON configuration required to construct a MariaDB destination.
+
 ```
+{
+  "connection": <connection-configuration>,
+  "writeMode": "create" | "replace" | "truncate"
+}
+```
+
+* `connection`: A [connection configuration](#connection-configuration) object.
+* `writeMode`: Indicates how to handle loading data into an existing table
+  * `create`: prevent loading data into an existing table, erroring if it exists
+  * `replace`: `DROP` and recreate an existing table prior to loading data
+  * `truncate`: `TRUNCATE` an existing table prior to loading data
+
+## Connection Configuration
+
+JSON configurating describing how to connect to MariaDB.
+
+```
+{
+  "jdbcUrl": String
+  [, "maxConcurrency": Number]
+  [, "maxLifetimeSecs": Number]
+}
+```
+
+* `jdbcUrl`: a MariaDB [connection string](https://mariadb.com/kb/en/about-mariadb-connector-j/#connection-strings). Note that any connection parameter values containing URI [reserved characters](https://tools.ietf.org/html/rfc3986#section-2.2) must be [percent encoded](https://tools.ietf.org/html/rfc3986#section-2.1) to avoid ambiguity.
+* `maxConcurrency` (optional): the maximum number of simultaneous connections to the database (default: 8)
+* `maxLifetimeSecs` (optional): the maximum lifetime, in seconds, of idle connections. If your database or infrastructure imposes any limit on idle connections, make sure to set this value to at most a few seconds less than the limit (default: 300 seconds)
---
+++ destination/src/test/scala/quasar/plugin/mariadb/destination/MariaDbDestinationSpec.scala
@@ -0,0 +1,339 @@
+/*
+ * Copyright 2020 Precog Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.plugin.mariadb.destination
+
+import scala.{text => _, Stream => _, _}, Predef._
+import scala.concurrent.ExecutionContext
+import scala.util.Random
+
+import java.time._
+import java.util.concurrent.Executors
+
+import cats.data.NonEmptyList
+import cats.effect.{Blocker, IO, Resource}
+import cats.effect.testing.specs2.CatsIO
+import cats.implicits._
+
+import doobie._
+import doobie.implicits._
+import doobie.implicits.javatime._
+
+import fs2._
+
+import org.slf4s.Logging
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.BeforeAll
+
+import quasar.api.Column
+import quasar.api.resource._
+import quasar.connector.{MonadResourceErr, ResourceError}
+import quasar.contrib.scalaz.MonadError_
+import quasar.plugin.jdbc.destination.WriteMode
+
+object MariaDbDestinationSpec extends Specification with CatsIO with BeforeAll with Logging {
+  import MariaDbType._
+  import Signedness._
+
+  implicit val ioMonadResourceErr: MonadResourceErr[IO] =
+    MonadError_.facet[IO](ResourceError.throwableP)
+
+  val TestDb: String = "precog_test"
+
+  val frag = Fragment.const0(_, None)
+
+  def TestUrl(db: Option[String]): String =
+    s"jdbc:mariadb://127.0.0.1:33306/${db getOrElse ""}?user=root&allowLocalInfile=true"
+
+  def TestXa(jdbcUrl: String): Resource[IO, Transactor[IO]] =
+    Resource.make(IO(Executors.newSingleThreadExecutor()))(p => IO(p.shutdown)) map { ex =>
+      Transactor.fromDriverManager(
+        "org.mariadb.jdbc.Driver",
+        jdbcUrl,
+        Blocker.liftExecutionContext(ExecutionContext.fromExecutorService(ex)))
+    }
+
+  def beforeAll(): Unit = {
+    TestXa(TestUrl(None))
+      .use(frag(s"CREATE DATABASE IF NOT EXISTS $TestDb").update.run.transact(_))
+      .void
+      .unsafeRunSync
+  }
+
+  def table(xa: Transactor[IO]): Resource[IO, (ResourcePath, String)] =
+    Resource.make(
+      IO(s"dest_spec_${Random.alphanumeric.take(6).mkString}"))(
+      name => frag(s"DROP TABLE IF EXISTS $name").update.run.transact(xa).void)
+      .map(n => (ResourcePath.root() / ResourceName(n), n))
+
+  def harnessed(
+      jdbcUrl: String = TestUrl(Some(TestDb)),
+      writeMode: WriteMode = WriteMode.Replace)
+      : Resource[IO, (Transactor[IO], MariaDbDestination[IO], ResourcePath, String)] =
+    for {
+      xa <- TestXa(jdbcUrl)
+      (path, name) <- table(xa)
+      dest = new MariaDbDestination(writeMode, xa, log)
+    } yield (xa, dest, path, name)
+
+  def csv(lines: String*): Stream[IO, Byte] =
+    Stream.emits(lines)
+      .intersperse("\r\n")
+      .through(text.utf8Encode)
+
+  def ingestValues[A: Read](tpe: MariaDbType, values: A*) = {
+    val input = csv(values.map(_.toString): _*)
+    val cols = NonEmptyList.one(Column("value", tpe))
+
+    harnessed(writeMode = WriteMode.Replace) use { case (xa, dest, path, tableName) =>
+      for {
+        _ <- dest.createSink.consume(path, cols, input).compile.drain
+        vals <- frag(s"select value from $tableName").query[A].to[List].transact(xa)
+      } yield {
+        vals must containTheSameElementsAs(values)
+      }
+    }
+  }
+
+  "write mode" >> {
+    val cols = NonEmptyList.one(Column("value", CHAR(1)))
+
+    "create" >> {
+      "succeeds when table absent" >> {
+        harnessed(writeMode = WriteMode.Create) use { case (xa, dest, path, tableName) =>
+          dest.createSink.consume(path, cols, csv("A", "B"))
+            .compile.drain.attempt.map(_ must beRight)
+        }
+      }
+
+      "fails when table present" >> {
+        harnessed(writeMode = WriteMode.Create) use { case (xa, dest, path, tableName) =>
+          for {
+            _ <- dest.createSink.consume(path, cols, csv("A", "B")).compile.drain
+            r <- dest.createSink.consume(path, cols, csv("A", "B")).compile.drain.attempt
+          } yield {
+            r must beLeft
+          }
+        }
+      }
+    }
+
+    "replace" >> {
+      "succeeds when table absent" >> {
+        harnessed(writeMode = WriteMode.Replace) use { case (xa, dest, path, tableName) =>
+          dest.createSink.consume(path, cols, csv("A", "B"))
+            .compile.drain.attempt.map(_ must beRight)
+        }
+      }
+
+      "succeeds when table present" >> {
+        harnessed(writeMode = WriteMode.Replace) use { case (xa, dest, path, tableName) =>
+          for {
+            _ <- dest.createSink.consume(path, cols, csv("A", "B")).compile.drain
+            r <- dest.createSink.consume(path, cols, csv("A", "B")).compile.drain.attempt
+          } yield {
+            r must beRight
+          }
+        }
+      }
+    }
+
+    "truncate" >> {
+      "succeeds when table absent" >> {
+        harnessed(writeMode = WriteMode.Truncate) use { case (xa, dest, path, tableName) =>
+          dest.createSink.consume(path, cols, csv("A", "B"))
+            .compile.drain.attempt.map(_ must beRight)
+        }
+      }
+
+      "succeeds when table present" >> {
+        harnessed(writeMode = WriteMode.Truncate) use { case (xa, dest, path, tableName) =>
+          for {
+            _ <- dest.createSink.consume(path, cols, csv("A", "B")).compile.drain
+            r <- dest.createSink.consume(path, cols, csv("A", "B")).compile.drain.attempt
+          } yield {
+            r must beRight
+          }
+        }
+      }
+    }
+  }
+
+  "ingest" >> {
+    "boolean" >> {
+      val input = csv("1", "0")
+      val cols = NonEmptyList.one(Column("thebool", BOOLEAN))
+
+      harnessed() use { case (xa, dest, path, tableName) =>
+        for {
+          _ <- dest.createSink.consume(path, cols, input).compile.drain
+
+          bools <-
+            frag(s"select thebool from $tableName")
+              .query[Boolean].to[List].transact(xa)
+        } yield {
+          bools must contain(true, false)
+        }
+      }
+    }
+
+    "number" >> {
+      "tinyint" >> ingestValues(TINYINT(SIGNED), -128, 0, 127)
+
+      "smallint" >> ingestValues(SMALLINT(SIGNED), -32768, 0, 32767)
+
+      "mediumint" >> ingestValues(MEDIUMINT(SIGNED), -8388608, 0, 8388607)
+
+      "int" >> ingestValues(INT(SIGNED), -2147483648, 0, 2147483647)
+
+      "bigint" >> ingestValues(BIGINT(SIGNED), -9223372036854775808L, 0L, 9223372036854775807L)
+
+      "float" >> ingestValues(FLOAT(SIGNED), -3.40282E+38f, Float.MinPositiveValue, 3.40282E+38f)
+
+      "double" >> ingestValues(DOUBLE(SIGNED), -1.7976931348623157E+308, 2.2250738585072014E-308, 1.7976931348623157E+308)
+
+      "decimal" >> {
+        val min = BigDecimal("-99999999999999999999999999999999999.999999999999999999999999999999")
+        val mid = BigDecimal("1234567898765432100000000.000000000099999966999977777799")
+        val max = BigDecimal("99999999999999999999999999999999999.999999999999999999999999999999")
+        ingestValues(DECIMAL(65, 30, SIGNED), min, mid, max)
+      }
+
+      "year" >> ingestValues(YEAR, 1901, 1969, 2155)
+    }
+
+    "string" >> {
+      List(TINYBLOB, BLOB, MEDIUMBLOB, LONGBLOB, TINYTEXT, TEXT, MEDIUMTEXT, LONGTEXT) foreach { tpe =>
+        tpe.toString.toLowerCase >> ingestValues(tpe, "føobår", "ひらがな", "b a t")
+      }
+
+      "nchar" >> ingestValues(CHAR(6), "føobår", "  ひらがな", " b a t")
+
+      "nvarchar" >> ingestValues(VARCHAR(6), "føobår", "ひらがな", "b a t")
+
+      "binary" >> ingestValues(BINARY(6), "foobar", "ひら", "føoba")
+
+      "varbinary" >> ingestValues(VARBINARY(6), "foobar", "ひら", "føo")
+    }
+
+    "localdate" >> {
+      val min = LocalDate.parse("1000-01-01")
+      val mid = LocalDate.parse("2020-07-15")
+      val max = LocalDate.parse("9999-12-31")
+
+      ingestValues(DATE, min, mid, max)
+    }
+
+    "localtime" >> {
+      val min = LocalTime.parse("00:00:00")
+      val mid = LocalTime.parse("12:11:32")
+      val midS = LocalTime.parse("13:42:32.123456")
+      val max = LocalTime.parse("23:59:59.999999")
+
+      ingestValues(TIME(6), min, mid, midS, max)
+    }
+
+    "localdatetime" >> {
+      val min = LocalDateTime.parse("1000-01-01T00:00:00.000000")
+      val mid = LocalDateTime.parse("2020-07-15T11:30:05")
+      val midS = LocalDateTime.parse("2020-05-05T03:30:45")
+      val max = LocalDateTime.parse("9999-12-31T23:59:59.999999")
+
+      ingestValues(DATETIME(6), min, mid, midS, max)
+    }
+
+    "containing special characters" >> {
+      val escA = """"foo,"",,""""""""
+      val escB = """"java""script""""""
+
+      val A = "foo,\",,\"\""
+      val B = "java\"script\""
+
+      val input = csv(escA, escB)
+      val cols = NonEmptyList.one(Column("value", VARCHAR(12)))
+
+      harnessed() use { case (xa, dest, path, tableName) =>
+        for {
+          _ <- dest.createSink.consume(path, cols, input).compile.drain
+
+          vals <-
+            frag(s"select value from $tableName")
+              .query[String].to[List].transact(xa)
+        } yield {
+          vals must contain(A, B)
+        }
+      }
+    }
+
+    "multiple fields" >> {
+      val row1 = "2020-07-12,123456.234234,\"hello world\",887798"
+      val row2 = "1983-02-17,732345.987,\"lorum, ipsum\",42"
+
+      val input = csv(row1, row2)
+
+      val cols = NonEmptyList.of(
+        Column("A", DATE),
+        Column("B", DOUBLE(SIGNED)),
+        Column("C", VARCHAR(20)),
+        Column("D", MEDIUMINT(SIGNED)))
+
+      harnessed() use { case (xa, dest, path, tableName) =>
+        for {
+          _ <- dest.createSink.consume(path, cols, input).compile.drain
+
+          vals <-
+            frag(s"select A, B, C, D from $tableName")
+              .query[(LocalDate, Double, String, Int)].to[List].transact(xa)
+        } yield {
+          vals must contain(
+            (LocalDate.parse("2020-07-12"), 123456.234234, "hello world", 887798),
+            (LocalDate.parse("1983-02-17"), 732345.987, "lorum, ipsum", 42))
+
+        }
+      }
+    }
+
+    "undefined fields" >> {
+      val row1 = "NULL,42,1992-03-04"
+      val row2 = "foo,NULL,1992-03-04"
+      val row3 = "foo,42,NULL"
+
+      val input = csv(row1, row2, row3)
+
+      val cols = NonEmptyList.of(
+        Column("A", CHAR(3)),
+        Column("B", TINYINT(UNSIGNED)),
+        Column("C", DATE))
+
+      harnessed() use { case (xa, dest, path, tableName) =>
+        for {
+          _ <- dest.createSink.consume(path, cols, input).compile.drain
+
+          vals <-
+            frag(s"select A, B, C from $tableName")
+              .query[(Option[String], Option[Int], Option[LocalDate])]
+              .to[List].transact(xa)
+        } yield {
+          vals must contain(
+            (None, Some(42), Some(LocalDate.parse("1992-03-04"))),
+            (Some("foo"), None, Some(LocalDate.parse("1992-03-04"))),
+            (Some("foo"), Some(42), None))
+        }
+      }
+    }
+  }
+}
